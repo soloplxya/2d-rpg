aed24e18b9080ac6ccf74ecbfb55fdf67d726efe
soloplxya <shainesnake@gmail.com>
2022-01-31 16:07:29 +0800

    add basic movements for input mappings

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -6,7 +6,6 @@ export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
+
+
 	
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,1 +1,27 @@
 extends Actor
+
+func _physics_process(delta: float) -> void:
+	var direction: = get_direction()
+	velocity = calculate_move_velocity(velocity, speed, direction)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
+func get_direction() -> Vector2: 
+	return Vector2(
+		# returns a floating point value -> fully pressed 1, not pressed 0
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-1.0 if Input.is_action_just_pressed("jump")  and is_on_floor() else 1.0
+	)
+		
+func calculate_move_velocity(
+	linear_velocity: Vector2,
+	speed: Vector2, 
+	direction: Vector2) -> Vector2:
+	var new_velocity: = linear_velocity 
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0: 
+		new_velocity.y = speed.y * direction.y
+	return new_velocity
+	
+	
